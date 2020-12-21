3b312516068513c946d32cd1bf006b9ec02e6f61
user <vsl_sv@bk.ru>
2020-12-21 16:41:47 +0300

    fix: rename method

--- src/fibonacci.cc
+++ src/fibonacci.cc
@@ -15,47 +15,47 @@ using v8::Value;
 // This is the implementation of the "add" method
 // Input arguments are passed using the const FunctionCallbackInfo<Value>& args struct
 void Fib(const FunctionCallbackInfo<Value>& args) {
-  Isolate* isolate = args.GetIsolate();
+    Isolate* isolate = args.GetIsolate();
 
-  // Check the number of arguments passed.
-  if (args.Length() != 1) {
+    // Check the number of arguments passed.
+    if (args.Length() != 1) {
     // Throw an Error that is passed back to JavaScript
     isolate->ThrowException(Exception::TypeError(
         String::NewFromUtf8(isolate,
                             "Wrong number of arguments",
                             NewStringType::kNormal).ToLocalChecked()));
     return;
-  }
+    }
 
-  // Check the argument types
-  if (!args[0]->IsNumber() || args[0].As<Number>()->Value() <= 0) {
-    isolate->ThrowException(Exception::TypeError(
-        String::NewFromUtf8(isolate,
+    // Check the argument types
+    if (!args[0]->IsNumber() || args[0].As<Number>()->Value() < 0) {
+        isolate->ThrowException(Exception::TypeError(
+            String::NewFromUtf8(isolate,
                             "Wrong arguments",
                             NewStringType::kNormal).ToLocalChecked()));
     return;
-  }
+    }
 
-  // Perform the operation
-  int n = args[0].As<Number>()->Value();
-  int a = 1;
-  int b = 1;
-  int i;
-  for (i = 3; i <= n; i++) {
-          int c = a + b;
-          a = b;
-          b = c;
-        }
-  int value = b;
+    // Perform the operation
+    // Get current element and count next one
+    int elem = args[0].As<Number>()->Value() + 1;
+    int prev = 0;
+    int next = 1;
+    for (int i = 0; i < elem; i++){
+        int temp = next;
+        next += prev;
+        prev = temp;
+    }
+    int value = prev;
 
-  Local<Number> num = Number::New(isolate, value);
+    Local<Number> num = Number::New(isolate, value);
 
-  // Set the return value (using the passed in FunctionCallbackInfo<Value>&)
-  args.GetReturnValue().Set(num);
+    // Set the return value (using the passed in FunctionCallbackInfo<Value>&)
+    args.GetReturnValue().Set(num);
 }
 
 void Init(Local<Object> exports) {
-  NODE_SET_METHOD(exports, "fib", Fib);
+    NODE_SET_METHOD(exports, "get", Fib);
 }
 
 NODE_MODULE(NODE_GYP_MODULE_NAME, Init)
